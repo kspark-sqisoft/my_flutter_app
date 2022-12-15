2a7155da325bcfc9dfa62ccfb4f6521fb385c32e
kspark-sqisoft <ks.park@sqisoft.com>
2022-12-15 16:10:12 +0900

    변경 완료

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/screens/product_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ProductScreen();
   }
 }
